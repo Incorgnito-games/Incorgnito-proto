08bbf658adf34e1d5e09ae37ca402d9deab675ad
catRoman-Laptop <catlinroman84@gmail.com>
2024-10-16 20:34:33 -0700

    added obstactles, developing ai interaction with state, added navigational mesh/ pathfinding to npc

---
+++ scripts/components/State/NPC/GoToWork.cs
@@ -0,0 +1,23 @@
+using Incorgnito.scripts.components.state;
+
+namespace Incorgnito.scripts.actors.npc;
+
+public partial class GoToWork: AbstractActionState
+{
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+    }
+
+    public override void UpdateProcess(double delta)
+    {
+    }
+
+    public override void UpdatePhysicsProcess(double delta)
+    {
+    }
+}
