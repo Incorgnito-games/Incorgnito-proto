1d9aa3b547068f42be9ec34a3d5d118e3fb1ce5d
catRoman-Laptop <catlinroman84@gmail.com>
2024-10-23 23:29:47 -0700

    add bus route/path/stop, more buildings

---
+++ scripts/objects/BusRoute.cs
@@ -0,0 +1,40 @@
+namespace Incorgnito.scripts.objects;
+using Godot;
+using System;
+
+public partial class BusRoute : PathFollow3D
+{
+	[Export] private float _busWaitTime;
+	[Export] private float _busSpeed;
+	private float _currentBusSpeed;
+	private Timer _busStopTimer;
+	public override void _Ready()
+	{
+		_busStopTimer = new Timer();
+		_busStopTimer.SetAutostart(false);
+		_busStopTimer.SetOneShot(false);
+		_busStopTimer.SetWaitTime(_busWaitTime);
+		_busStopTimer.Timeout += OnBusStopTimeout;
+		AddChild(_busStopTimer);
+		_currentBusSpeed = _busSpeed;
+
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		
+		this.Progress += _currentBusSpeed;
+	}
+
+	public void OnBusStopTimeout()
+	{
+		GD.Print("Signame Entered");
+		_currentBusSpeed = _busSpeed;
+	}
+
+	public void OnArea3dBodyEntered(Node body)
+	{
+		_currentBusSpeed = 0;
+		_busStopTimer.Start();	
+	}
+}
